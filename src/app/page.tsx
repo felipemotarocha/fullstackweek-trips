b5d85a147ea9b0c9650d07f12d15283046f7e8b5
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-04 20:53:40 -0300

    feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+"use client";
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return <div>hello world</div>;
 }
