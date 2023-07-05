af68c0e36b855e5b61df26cb7af196462fe98044
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-04 22:12:50 -0300

    feat: add quick search component

---
+++ src/app/components/QuickSearch.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import React from "react";
+
+const QuickSearch = () => {
+  return (
+    <div className="container mx-auto p-5">
+      <div className="flex items-center">
+        <div className="w-full h-[1px] bg-grayLighter"></div>
+        <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">Tente pesquisar por</h2>
+        <div className="w-full h-[1px] bg-grayLighter"></div>
+      </div>
+
+      <div className="flex w-full justify-between mt-5">
+        <div className="flex flex-col items-center gap-1">
+          <Image width={35} height={35} src="/hotel-icon.png" alt="Hotel" />
+
+          <p className="text-sm text-grayPrimary">Hotel</p>
+        </div>
+
+        <div className="flex flex-col items-center gap-1">
+          <Image width={35} height={35} src="/farm-icon.png" alt="Hotel" />
+
+          <p className="text-sm text-grayPrimary">Fazenda</p>
+        </div>
+
+        <div className="flex flex-col items-center gap-1">
+          <Image width={35} height={35} src="/cottage-icon.png" alt="Hotel" />
+
+          <p className="text-sm text-grayPrimary">Chalé</p>
+        </div>
+
+        <div className="flex flex-col items-center gap-1">
+          <Image width={35} height={35} src="/inn-icon.png" alt="Hotel" />
+
+          <p className="text-sm text-grayPrimary">Pousada</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuickSearch;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import QuickSearch from "./components/QuickSearch";
 import TripSearch from "./components/TripSearch";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
+      <QuickSearch />
     </div>
   );
 }
