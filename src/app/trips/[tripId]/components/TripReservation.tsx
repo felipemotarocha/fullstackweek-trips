f523defb1a7a4b405bf7616ea784b07cc59d6454
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-05 20:37:02 -0300

    feat: add trip reservation component

---
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Button from "@/components/Button";
+import DatePicker from "@/components/DatePicker";
+import Input from "@/components/Input";
+import { Trip } from "@prisma/client";
+import React from "react";
+
+interface TripReservationProps {
+  trip: Trip;
+}
+
+const TripReservation = ({ trip }: TripReservationProps) => {
+  return (
+    <div>
+      <div className="flex flex-col px-5">
+        <div className="flex gap-4">
+          <DatePicker placeholderText="Data de Início" onChange={() => {}} className="w-full" />
+          <DatePicker placeholderText="Data Final" onChange={() => {}} className="w-full" />
+        </div>
+
+        <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} className="mt-4" />
+
+        <div className="flex justify-between mt-3">
+          <p className="font-medium text-sm text-primaryDarker">Total: </p>
+          <p className="font-medium text-sm text-primaryDarker">R$2500</p>
+        </div>
+
+        <Button className="mt-3">Reservar agora</Button>
+      </div>
+    </div>
+  );
+};
+
+export default TripReservation;
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 
 import React from "react";
 import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -23,6 +24,7 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
       <TripHeader trip={trip} />
 
       {/* RESERVA */}
+      <TripReservation trip={trip} />
     </div>
   );
 };
