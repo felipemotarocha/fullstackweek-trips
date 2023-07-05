89146c979035760dffd9cc12e35663e81b721849
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-04 21:55:02 -0300

    chore: add domains to next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
