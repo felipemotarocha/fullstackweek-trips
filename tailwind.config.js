160dc0375df10b9dd5dc65112d40a76be46fa0ae
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-04 21:54:44 -0300

    feat: add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#590BD8",
+        primaryLighter: "#DDD5EA",
+        primaryDarker: "#312A4F",
+        grayPrimary: "#717171",
+        grayLighter: "#BBBFBF",
+      },
+      textColor: {
+        dark: "#717171",
       },
     },
   },
   plugins: [],
-}
+};
