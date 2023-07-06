523bea4018a6513a7107b49142fa53ed982ac8f9
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-05 21:07:53 -0300

    feat: add button variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,17 @@
 import { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-function Button({ className, ...props }: ComponentPropsWithoutRef<"button">) {
-  const _className = twMerge(
-    "appearance-none rounded-lg bg-primary p-2 text-sm font-medium text-white shadow transition-all hover:bg-primaryDarker",
-    className
-  );
+interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
+  variant: "primary" | "outlined";
+}
+
+function Button({ className, variant = "primary", ...props }: ButtonProps) {
+  const variantClasses = {
+    primary: "bg-primary text-white hover:bg-primaryDarker",
+    outlined: "bg-transparent border-2 border-primary text-primary",
+  };
+
+  const _className = twMerge(variantClasses[variant], "appearance-none rounded-lg p-2 text-sm font-medium shadow transition-all", className);
 
   return (
     <button className={_className} {...props}>
