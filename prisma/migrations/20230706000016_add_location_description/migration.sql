769cb932bb6aca82173f83169e026bc40ce768b9
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-05 21:07:32 -0300

    feat: add location description

---
+++ prisma/migrations/20230706000016_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
