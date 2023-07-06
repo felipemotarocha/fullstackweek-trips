9e1d418741b7a7f74025bdc31d89ab0f99b8fed5
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-06 20:21:08 -0300

    feat: add validation to guests input

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -51,7 +51,7 @@ const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, priceP
         message: "Esta data já está reservada.",
       });
 
-      setError("endDate", {
+      return setError("endDate", {
         type: "manual",
         message: "Esta data já está reservada.",
       });
@@ -65,7 +65,7 @@ const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, priceP
     }
 
     if (res?.error?.code === "INVALID_END_DATE") {
-      setError("endDate", {
+      return setError("endDate", {
         type: "manual",
         message: "Data inválida.",
       });
@@ -130,11 +130,16 @@ const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, priceP
             value: true,
             message: "Número de hóspedes é obrigatório.",
           },
+          max: {
+            value: maxGuests,
+            message: `Número de hóspedes não pode ser maior que ${maxGuests}.`,
+          },
         })}
         placeholder={`Número de hóspedes (max: ${maxGuests})`}
         className="mt-4"
         error={!!errors?.guests}
         errorMessage={errors?.guests?.message}
+        type="number"
       />
 
       <div className="flex justify-between mt-3">
