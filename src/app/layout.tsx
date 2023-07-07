b4fb684837c60aa6ac72f214043d499423ea4260
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-07 20:51:00 -0300

    style: footer style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={poppins.className}>
         <NextAuthProvider>
           <ToastProvider>
-            <Header />
+            <div className="flex flex-col h-screen">
+              <div className="h-[94px]">
+                <Header />
+              </div>
 
-            {children}
+              <div className="flex-1">{children}</div>
 
-            <Footer />
+              <Footer />
+            </div>
           </ToastProvider>
         </NextAuthProvider>
       </body>
