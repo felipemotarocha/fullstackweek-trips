ef89c0578acdbe75fa5a08c091145c6885668ccd
Felipe Rocha <felipemota.rocha@gmail.com>
2023-07-07 21:37:22 -0300

    style: adapt trip confirmation to desktop

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -90,7 +90,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const guests = searchParams.get("guests");
 
   return (
-    <div className="container mx-auto p-5">
+    <div className="container mx-auto p-5 lg:max-w-[600px]">
       <h1 className="font-semibold text-xl text-primaryDarker">Sua viagem</h1>
 
       {/* CARD */}
